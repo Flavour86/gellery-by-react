49873febd375da6ae0f9b952972ec4b46ee38310
江典恩 <982624@nd.com>
2016-05-24 20:05:33 +0800

    component update

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,27 +13,116 @@ class AppComponent extends React.Component {
   }
 
   componentWillMount() {
-    this.resetItemList(this.path, imageDatas)
+    this.getItemImgUrl(this.path, imageDatas)
   }
 
-  resetItemList(path, data) {
+  getItemImgUrl(path, data) {
     if (!data) return
-    data.map(item => {
+    data.map((item, key) => {
+      const rotate = this.getRangeRotate();
       item.imageUrl = path + item.fileName
+      if (key === this.state.activeIndex) {
+        item.style = {
+          transform: 'rotate(0)'
+        }
+      } else {
+        item.style = {
+          transform: `rotate(${rotate}deg)`
+        }
+      }
     })
      this.setState({
        dataList: data
      })
   }
 
+  componentDidMount() {
+    this.resetItemList(this.state.dataList, this.state.activeIndex)
+  }
+
+  resetItemList(data, active) {
+    if (!data) return
+    const stageWidth = this.refs.stage.scrollWidth,
+           stageHeight = this.refs.stage.scrollHeight;
+    const imgWidth = this.refs.imgFigure0.scrollWidth,
+           imgHeight = this.refs.imgFigure0.scrollHeight;
+    const leftInterval = {
+      x: {
+        min: 0,
+        max: stageWidth/2 - imgWidth*2
+      },
+      y: {
+        min: 0,
+        max: stageHeight - imgHeight
+      }
+    }
+    const rightInterval = {
+      x: {
+        min: stageWidth/2 + imgWidth,
+        max: stageWidth - imgWidth
+      },
+      y:{
+        min: 0,
+        max: stageHeight - imgHeight
+      }
+    }
+    const centerInterval = {
+      x: {
+        min: stageWidth/2 - imgWidth,
+        max: stageWidth/2 + imgWidth/2
+      },
+      y:{
+        min: 0,
+        max: stageHeight/2 - imgHeight
+      }
+    }
+    let centerPos = data.splice(active, 1);
+    let topPos = this.getRangeArrNum(data,1);
+    let leftPos = this.getRangeArrNum(data, Math.ceil(data.length / 2));
+    let rightPos = data;
+    this.getItemPosi(topPos, centerInterval)
+    this.getItemPosi(leftPos, leftInterval)
+    this.getItemPosi(rightPos, rightInterval)
+    if (centerPos.length !==0) {
+      centerPos[0].style.left = stageWidth/2 - imgWidth/2 + 'px'
+      centerPos[0].style.top = stageHeight/2 + 'px'
+    }
+    const newData = [...centerPos,...topPos,...leftPos,...rightPos]
+    this.setState({
+      dataList: newData
+    })
+  }
+
+  getItemPosi(data, pos) {
+    if (!data.length || !pos) return;
+    data.map(item => {
+      item.style.left = this.getRangeNum(pos.x.min, pos.x.max) + 'px'
+      item.style.top = this.getRangeNum(pos.y.min, pos.y.max) + 'px'
+    })
+  }
+  getRangeRotate() {
+    return (Math.random() > 0.5 ? '-' : '') + Math.ceil(Math.random()*45)
+  }
+
+  getRangeArrNum(arr, num) {
+    let temp = [];
+    for(let i=0;i<num;i++) {
+      let index = this.getRangeNum(0, arr.length-1);
+      temp = temp.concat(arr.splice(index, 1))
+    }
+    return temp
+  }
+
+  getRangeNum(low, high) {
+    return Math.ceil(Math.random() * (high - low) + low)
+  }
+
   getImgFigure(data) {
     if (!data) return null
     let tempArr = []
     let imgFigureClassName = 'img-figure';
-    let styleObj = {
-    }
     data.map((item, key) => {
-      tempArr.push(<figure className={imgFigureClassName} key={key} style={styleObj}>
+      tempArr.push(<figure className={imgFigureClassName} key={key} ref={`imgFigure${key}`} style={item.style}>
         <img src={item.imageUrl}
              alt={item.title}
         />
@@ -54,7 +143,7 @@ class AppComponent extends React.Component {
     const imgFigure = this.getImgFigure(dataList)
     return (
       <div className="content">
-        <section className="stage">
+        <section className="stage" ref="stage">
           <section className="img-sec">
             {imgFigure}
           </section>
